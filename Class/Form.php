c9accd10496b300740fba9facde487bbd409ff2d
houdret <jeanhoudret2006@hotmail.com>
2021-11-26 13:18:32 +0100

    exe 1

---
+++ Class/Form.php
@@ -0,0 +1,111 @@
+<?php
+
+class Form
+{
+    public $data;
+
+    /**
+     * @param $data
+     */
+    /**
+     * @var string tag utilisé pour entouré les champs
+     */
+    public $surround = 'p';
+
+    public function __construct($data = array())
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function input($name){
+        return $this->surround(
+            '<label for="'.$name.'" >Votre Nom </label><input type="text" name="'.$name.'" value="'.$this->getValue($name).'">'
+        );
+    }
+
+    /**
+     * @return string choix de selection
+     */
+    public function select(){
+        return $this->surround(
+            '<label for="1" >Votre sélection </label>
+                   <select aria-label="Default select example">
+                      <option selected>Open this select menu</option>
+                      <option value=""></option>
+                      <option value=""></option>
+                      <option value=""></option>
+                </select>'
+        );
+    }
+
+    /**
+     * @return string checkbox
+     */
+    public function checkbox(){
+        return $this->surround(
+            '<input type="checkbox" value="" id="">
+                   <label for="" >Example </label>'
+        );
+    }
+
+    /**
+     * @return string commentaire
+     */
+    public function textarea(){
+        return $this->surround(
+            '<label for="" >Commentaire </label><textarea  id=""></textarea>'
+        );
+    }
+
+    /**
+     * @return string radio bouton
+     */
+    public function btnRadio(){
+        return $this->surround(
+            '<input type="radio" name="" id="">
+                   <label for="" >Example </label>'
+        );
+    }
+    /**
+     * @return mixed
+     */
+    public function submit(){
+        return $this->surround('<button type="submit" >Envoyer</button>');
+    }
+
+    /**
+     * @param $action
+     * @return string
+     */
+    public function create($action){
+        return $this->surround(
+            '<form method="" action="'.$action.'">'
+        );
+    }
+
+    public function endForm(){
+        return $this->surround(
+            '</form">'
+        );
+    }
+    /**
+     * @param $html string code html entouré
+     * @return string
+     */
+    protected function surround($html){
+        return "<{$this->surround}>{$html}</{$this->surround}>";
+    }
+
+    /**
+     * @param $index
+     * @return mixed|null
+     */
+    protected  function getValue($index){
+        return isset($this->data[$index]) ? $this->data[$index] : null;
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="création d'un formulaire en utilisant des classe">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Formulaire via Classe</title>
+</head>
+<body>
+<div class="container">
+    <?php
+    require 'Class/Form.php';
+
+
+
+    $form = new Form($_POST);
+
+    echo '<br/';
+    echo $form->create('$_POST');
+    echo $form->input('username');
+    echo $form->select();
+    echo $form->checkbox();
+    echo $form->btnRadio();
+    echo $form->textarea();
+    echo $form->submit();
+    echo $form->endForm();
+    ?>
+
+</div>
+
+
+</body>
+</html>
+
+
